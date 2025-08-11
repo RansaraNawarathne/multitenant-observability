e0a5ab5d5ed7ad3ce35f41d19f7d4e795f6f0eec
Malindu Nawarathne <ransaranawarathne@gmail.com>
2025-08-11 20:25:06 +0530

    docs: add Javadoc to CreateAgentRequestDTO

--- src/main/java/com/aurora/observability/dto/CreateAgentRequestDTO.java
+++ src/main/java/com/aurora/observability/dto/CreateAgentRequestDTO.java
@@ -8,12 +8,43 @@ import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
+/**
+ * Data Transfer Object (DTO) used for creating a new agent in the observability system.
+ * <p>
+ * This class is typically used as the request payload in API calls that
+ * create and register a new agent. It contains the necessary metadata
+ * to define the agent's identity, type, and expiration details.
+ * </p>
+ *
+ * <p><b>Usage Example:</b></p>
+ * <pre>
+ *     CreateAgentRequestDTO request = CreateAgentRequestDTO.builder()
+ *         .name("TelemetryAgent-001")
+ *         .type(AgentType.TELEMETRY)
+ *         .expireDate(new Date())
+ *         .build();
+ * </pre>
+ *
+ * @see AgentType
+ */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class CreateAgentRequestDTO {
+
+    /**
+     * Human-readable name of the agent to be created.
+     */
     private String name;
+
+    /**
+     * The functional category or role of the agent.
+     */
     private AgentType type;
+
+    /**
+     * The date and time when the agent's validity or subscription will expire.
+     */
     private Date expireDate;
 }
